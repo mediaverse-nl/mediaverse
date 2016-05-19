b706d42d9667721b3289b918d24cd66b5965e631
Deveron Reniers <def-lol-lol@hotmail.com>
2016-05-19 21:44:22 +0200

    update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::auth();
+
+//Route::get('/', 'HomeController@index');
+Route::get('/', function () { return view('home'); });
+Route::get('/algemene-voorwaarden', function () { return view('algemene-voorwaarden'); });
+Route::get('/applicaties', function () { return view('applicaties'); });
+Route::get('/contact', function () { return view('contact'); });
+Route::get('/diensten', function () { return view('diensten'); });
+Route::get('/over-ons', function () { return view('over-ons'); });
+Route::get('/privacy-verklaring', function () { return view('privacy-verklaring'); });
+Route::get('/referenties', function () { return view('referenties'); });
+Route::get('/seo', function () { return view('seo'); });
+Route::get('/sitemap', function () { return view('sitemap'); });
+Route::get('/webshops', function () { return view('webshops'); });
+Route::get('/websites', function () { return view('websites'); });
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,25 @@
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4 text-center">
+            <div class="center-block" style="border-radius: 50%; background: #BADA55; width: 200px; height: 200px; overflow: hidden">
+                <a href="{{ url('websites') }}">Websites</a>
+                <img src="#">
+            </div>
+        </div>
+        <div class="col-lg-4 text-center">
+            <div class="center-block" style="border-radius: 50%; background: #BADA55; width: 200px; height: 200px; overflow: hidden">
+                <a href="{{ url('seo') }}">SEO</a>
+                <img src="#">
+            </div>
+        </div>
+        <div class="col-lg-4 text-center">
+            <div class="center-block" style="border-radius: 50%; background: #BADA55; width: 200px; height: 200px; overflow: hidden">
+                <a href="{{ url('webshops') }}">Webshops</a>
+                <img src="#">
+            </div>
+        </div>
+    </div>
+</div>
+
+
